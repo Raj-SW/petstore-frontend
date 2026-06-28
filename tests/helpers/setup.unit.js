5ca2f79380cc37baf46d4ff39beb4b13b3f070c2
Raj-SW <nsseetohul@gmail.com>
2026-06-28 18:35:22 +0400

    test(unit): move Vitest config to vitest.config.js with unit+integration projects

---
+++ tests/helpers/setup.unit.js
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+
+// jsdom doesn't implement IntersectionObserver (used by framer-motion whileInView).
+if (typeof globalThis.IntersectionObserver === "undefined") {
+  globalThis.IntersectionObserver = class IntersectionObserver {
+    constructor(callback) {
+      this.callback = callback;
+    }
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+    takeRecords() {
+      return [];
+    }
+  };
+}
